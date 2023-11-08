21cd0a42cea924a86112a43e1dfdfa395eed2cbe
Kyle Barron <kyle@developmentseed.org>
2023-11-08 12:32:31 -0500

    wip

--- src/h3-hexagon-layer.ts
+++ src/h3-hexagon-layer.ts
@@ -39,53 +39,194 @@ import {
 import { EXTENSION_NAME } from "./constants.js";
 
 /** All properties supported by GeoArrowH3HexagonLayer */
-export type GeoArrowH3HexagonLayerProps<DataT = unknown> =
-  _GeoArrowH3HexagonLayerProps<DataT> & PolygonLayerProps<DataT>;
+export type GeoArrowH3HexagonLayerProps = Omit<
+  H3HexagonLayerProps,
+  "data" | "getHexagon"
+> &
+  _GeoArrowH3HexagonLayerProps &
+  CompositeLayerProps;
 
 /** Props added by the GeoArrowH3HexagonLayer */
-type _GeoArrowH3HexagonLayerProps<DataT> = Omit<H3HexagonLayerProps, "getHexagon"> & {
-  getHexagon: arrow.Vector<arrow.Utf8>;
+type _GeoArrowH3HexagonLayerProps = {
+  data?: arrow.Table;
 
-}
-
-let x: _GeoArrowH3HexagonLayerProps;
-function tmp(x: _GeoArrowH3HexagonLayerProps<unknown>) {
-  x.gethe
-}
-
-
-{
-  /**
-   * Whether or not draw hexagons with high precision.
-   * @default 'auto'
-   */
-  highPrecision?: boolean | "auto";
-  /**
-   * Coverage of hexagon in cell.
-   * @default 1
-   */
-  coverage?: number;
-  /**
-   * Center hexagon that best represents the shape of the set. If not specified, the hexagon closest to the center of the viewport is used.
-   */
-  centerHexagon?: string | null; // H3Index | null;
   /**
    * Called for each data object to retrieve the quadkey string identifier.
-   *
-   * By default, it reads `hexagon` property of data object.
    */
   getHexagon: arrow.Vector<arrow.Utf8>;
+
   /**
-   * Whether to extrude polygons.
+   * If `true`, validate the arrays provided (e.g. chunk lengths)
    * @default true
    */
-  extruded?: boolean;
+  _validate?: boolean;
 };
 
+// Remove data from the upstream default props
+const {
+  data: _data,
+  getHexagon: _getHexagon,
+  ..._defaultProps
+} = H3HexagonLayer.defaultProps;
+
 const defaultProps: DefaultProps<GeoArrowH3HexagonLayerProps> = {
-  ...PolygonLayer.defaultProps,
-  highPrecision: "auto",
-  coverage: { type: "number", min: 0, max: 1, value: 1 },
-  centerHexagon: null,
-  extruded: true,
+  ..._defaultProps,
+  _validate: true,
 };
+
+// export class GeoArrowArcLayer<
+//   ExtraProps extends {} = {}
+// > extends CompositeLayer<Required<GeoArrowArcLayerProps> & ExtraProps> {
+//   static defaultProps = defaultProps;
+//   static layerName = "GeoArrowArcLayer";
+
+//   getPickingInfo({
+//     info,
+//     sourceLayer,
+//   }: GetPickingInfoParams): GeoArrowPickingInfo {
+//     const { data: table } = this.props;
+
+//     // Geometry index as rendered
+//     let index = info.index;
+
+//     // @ts-expect-error `recordBatchIdx` is manually set on layer props
+//     const recordBatchIdx: number = sourceLayer.props.recordBatchIdx;
+//     const batch = table.batches[recordBatchIdx];
+//     const row = batch.get(index);
+
+//     // @ts-expect-error hack: using private method to avoid recomputing via
+//     // batch lengths on each iteration
+//     const offsets: number[] = table._offsets;
+//     const currentBatchOffset = offsets[recordBatchIdx];
+
+//     // Update index to be _global_ index, not within the specific record batch
+//     index += currentBatchOffset;
+//     return {
+//       ...info,
+//       index,
+//       object: row,
+//     };
+//   }
+
+//   renderLayers(): Layer<{}> | LayersList | null {
+//     return this._renderLayersPoint();
+//   }
+
+//   _renderLayersPoint(): Layer<{}> | LayersList | null {
+//     const {
+//       data: table,
+//       getSourcePosition: sourcePosition,
+//       getTargetPosition: targetPosition,
+//     } = this.props;
+
+//     if (this.props._validate) {
+//       const vectorAccessors: arrow.Vector[] = [sourcePosition, targetPosition];
+//       for (const accessor of [
+//         this.props.getSourceColor,
+//         this.props.getTargetColor,
+//         this.props.getWidth,
+//         this.props.getHeight,
+//         this.props.getTilt,
+//       ]) {
+//         if (accessor instanceof arrow.Vector) {
+//           vectorAccessors.push(accessor);
+//         }
+//       }
+
+//       validatePointType(sourcePosition.type);
+//       validatePointType(targetPosition.type);
+//       if (table) {
+//         validateVectorAccessors(table, vectorAccessors);
+//       } else {
+//         const validationTable = new arrow.Table({
+//           source: sourcePosition,
+//           target: targetPosition,
+//         });
+//         validateVectorAccessors(validationTable, vectorAccessors);
+//       }
+
+//       if (this.props.getSourceColor instanceof arrow.Vector) {
+//         validateColorVector(this.props.getSourceColor);
+//       }
+//       if (this.props.getTargetColor instanceof arrow.Vector) {
+//         validateColorVector(this.props.getTargetColor);
+//       }
+//     }
+
+//     const layers: H3HexagonLayer[] = [];
+//     for (
+//       let recordBatchIdx = 0;
+//       recordBatchIdx < table.batches.length;
+//       recordBatchIdx++
+//     ) {
+//       const sourceData = sourcePosition.data[recordBatchIdx];
+//       const sourceValues = getPointChild(sourceData).values;
+//       const targetData = targetPosition.data[recordBatchIdx];
+//       const targetValues = getPointChild(targetData).values;
+
+//       const props: H3HexagonLayerProps = {
+//         // @ts-expect-error used for picking purposes
+//         recordBatchIdx,
+
+//         id: `${this.props.id}-geoarrow-arc-${recordBatchIdx}`,
+
+//         greatCircle: this.props.greatCircle,
+//         numSegments: this.props.numSegments,
+//         widthUnits: this.props.widthUnits,
+//         widthScale: this.props.widthScale,
+//         widthMinPixels: this.props.widthMinPixels,
+//         widthMaxPixels: this.props.widthMaxPixels,
+
+//         data: {
+//           length: sourceData.length,
+//           attributes: {
+//             getSourcePosition: {
+//               value: sourceValues,
+//               size: sourceData.type.listSize,
+//             },
+//             getTargetPosition: {
+//               value: targetValues,
+//               size: targetData.type.listSize,
+//             },
+//           },
+//         },
+//       };
+
+//       assignAccessor({
+//         props,
+//         propName: "getSourceColor",
+//         propInput: this.props.getSourceColor,
+//         chunkIdx: recordBatchIdx,
+//       });
+//       assignAccessor({
+//         props,
+//         propName: "getTargetColor",
+//         propInput: this.props.getTargetColor,
+//         chunkIdx: recordBatchIdx,
+//       });
+//       assignAccessor({
+//         props,
+//         propName: "getWidth",
+//         propInput: this.props.getWidth,
+//         chunkIdx: recordBatchIdx,
+//       });
+//       assignAccessor({
+//         props,
+//         propName: "getHeight",
+//         propInput: this.props.getHeight,
+//         chunkIdx: recordBatchIdx,
+//       });
+//       assignAccessor({
+//         props,
+//         propName: "getTilt",
+//         propInput: this.props.getTilt,
+//         chunkIdx: recordBatchIdx,
+//       });
+
+//       const layer = new H3HexagonLayer(this.getSubLayerProps(props));
+//       layers.push(layer);
+//     }
+
+//     return layers;
+//   }
+// }
