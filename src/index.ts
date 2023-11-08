df3da43ee9e9d1fbf8d2af6db76b23d4c1467393
Kyle Barron <kyle@developmentseed.org>
2023-11-08 13:32:28 -0500

    export hex layer with underscore

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { GeoArrowArcLayer } from "./arc-layer.js";
 export { GeoArrowColumnLayer } from "./column-layer.js";
-export { GeoArrowH3HexagonLayer } from "./h3-hexagon-layer.js";
+export { GeoArrowH3HexagonLayer as _GeoArrowH3HexagonLayer } from "./h3-hexagon-layer.js";
 export { GeoArrowHeatmapLayer } from "./heatmap-layer.js";
 export { GeoArrowPathLayer } from "./path-layer.js";
 export { GeoArrowScatterplotLayer } from "./scatterplot-layer.js";
@@ -8,7 +8,7 @@ export { GeoArrowSolidPolygonLayer } from "./solid-polygon-layer.js";
 
 export type { GeoArrowArcLayerProps } from "./arc-layer.js";
 export type { GeoArrowColumnLayerProps } from "./column-layer.js";
-export type { GeoArrowH3HexagonLayerProps } from "./h3-hexagon-layer.js";
+export type { GeoArrowH3HexagonLayerProps as _GeoArrowH3HexagonLayerProps } from "./h3-hexagon-layer.js";
 export type { GeoArrowHeatmapLayerProps } from "./heatmap-layer.js";
 export type { GeoArrowPathLayerProps } from "./path-layer.js";
 export type { GeoArrowScatterplotLayerProps } from "./scatterplot-layer.js";
