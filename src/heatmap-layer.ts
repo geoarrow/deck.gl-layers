a7eb294e3fa9e3c74a1db9f228ac472633fdf1fc
Kyle Barron <kyle@developmentseed.org>
2023-11-08 12:21:17 -0500

    Simplify props definition and typing (#65)

--- src/heatmap-layer.ts
+++ src/heatmap-layer.ts
@@ -1,5 +1,4 @@
 import {
-  Color,
   CompositeLayer,
   CompositeLayerProps,
   DefaultProps,
@@ -20,82 +19,17 @@ import {
 import { FloatAccessor, PointVector } from "./types.js";
 import { EXTENSION_NAME } from "./constants.js";
 
-const defaultColorRange: [number, number, number][] = [
-  [255, 255, 178],
-  [254, 217, 118],
-  [254, 178, 76],
-  [253, 141, 60],
-  [240, 59, 32],
-  [189, 0, 38],
-];
-
 /** All properties supported by GeoArrowHeatmapLayer */
-export type GeoArrowHeatmapLayerProps = _GeoArrowHeatmapLayerProps &
+export type GeoArrowHeatmapLayerProps = Omit<
+  HeatmapLayerProps,
+  "getPosition" | "getWeight"
+> &
+  _GeoArrowHeatmapLayerProps &
   CompositeLayerProps;
 
 /** Properties added by GeoArrowHeatmapLayer */
 type _GeoArrowHeatmapLayerProps = {
   data?: arrow.Table;
-
-  /**
-   * Radius of the circle in pixels, to which the weight of an object is distributed.
-   *
-   * @default 30
-   */
-  radiusPixels?: number;
-
-  /**
-   * Specified as an array of colors [color1, color2, ...].
-   *
-   * @default `6-class YlOrRd` - [colorbrewer](http://colorbrewer2.org/#type=sequential&scheme=YlOrRd&n=6)
-   */
-  colorRange?: Color[];
-
-  /**
-   * Value that is multiplied with the total weight at a pixel to obtain the final weight.
-   *
-   * @default 1
-   */
-  intensity?: number;
-
-  /**
-   * Ratio of the fading weight to the max weight, between `0` and `1`.
-   *
-   * For example, `0.1` affects all pixels with weight under 10% of the max.
-   *
-   * Ignored when `colorDomain` is specified.
-   * @default 0.05
-   */
-  threshold?: number;
-
-  /**
-   * Controls how weight values are mapped to the `colorRange`, as an array of two numbers [`minValue`, `maxValue`].
-   *
-   * @default null
-   */
-  colorDomain?: [number, number] | null;
-
-  /**
-   * Defines the type of aggregation operation
-   *
-   * V valid values are 'SUM', 'MEAN'.
-   *
-   * @default 'SUM'
-   */
-  aggregation?: "SUM" | "MEAN";
-
-  /**
-   * Specifies the size of weight texture.
-   * @default 2048
-   */
-  weightsTextureSize?: number;
-
-  /**
-   * Interval in milliseconds during which changes to the viewport don't trigger aggregation.
-   *
-   * @default 500
-   */
-  debounceTimeout?: number;
 
   /**
    * Method called to retrieve the position of each object.
@@ -118,19 +52,16 @@ type _GeoArrowHeatmapLayerProps = {
   _validate?: boolean;
 };
 
-const defaultProps: DefaultProps<GeoArrowHeatmapLayerProps> = {
-  _validate: true,
+// Remove data from the upstream default props
+const {
+  data: _data,
+  getPosition: _getPosition,
+  ..._defaultProps
+} = HeatmapLayer.defaultProps;
 
-  getWeight: { type: "accessor", value: 1 },
-  intensity: { type: "number", min: 0, value: 1 },
-  radiusPixels: { type: "number", min: 1, max: 100, value: 50 },
-  colorRange: defaultColorRange,
-  threshold: { type: "number", min: 0, max: 1, value: 0.05 },
-  colorDomain: { type: "array", value: null, optional: true },
-  // 'SUM' or 'MEAN'
-  aggregation: "SUM",
-  weightsTextureSize: { type: "number", min: 128, max: 2048, value: 2048 },
-  debounceTimeout: { type: "number", min: 0, max: 1000, value: 500 },
+const defaultProps: DefaultProps<GeoArrowHeatmapLayerProps> = {
+  ..._defaultProps,
+  _validate: true,
 };
 
 export class GeoArrowHeatmapLayer<
