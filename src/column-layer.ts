47d7a7f2b4b7a835da43dcb11a413b8567b8dd7e
Kyle Barron <kyle@developmentseed.org>
2023-11-22 18:14:17 -0500

    Enable strict null checks (#80)
    * Enable strict null checks
    
    * Fix type errors

--- src/column-layer.ts
+++ src/column-layer.ts
@@ -89,6 +89,7 @@ const ourDefaultProps = {
   _validate: true,
 };
 
+// @ts-expect-error Type error in merging default props with ours
 const defaultProps: DefaultProps<GeoArrowColumnLayerProps> = {
   ..._defaultProps,
   ...ourDefaultProps,
