2cba8cf8660623a7a89d550b2000867e706cb464
Kyle Barron <kyle@developmentseed.org>
2025-10-09 16:43:34 -0400

    repo refactor / fix npm workspace setup (#150)
    * Move into packages/ folder
    
    * Move back to examples folder
    
    * move tests
    
    * move rollup config
    
    * workspace dep
    
    * specify overrides
    
    * fix fmt in ci
    
    * Working point demo
    
    * docs
    
    * move tsconfig to root
    
    * Fix readme instructions
    
    * remove yarn.lock files

--- examples/linestring/README.md
+++ examples/linestring/README.md
@@ -4,15 +4,10 @@ Uses [Vite](https://vitejs.dev/) to bundle and serve files.
 
 ## Usage
 
-To install dependencies:
-
-```bash
-npm install
-# or
-yarn
-```
+To install dependencies, run `npm install` at the top level of this workspace, not in this directory.
 
 Commands:
+
 * `npm start` is the development target, to serve the app and hot reload.
 * `npm run build` is the production target, to create the final bundle and write to disk.
 
---
+++ packages/deck.gl-layers/src/index.ts
@@ -0,0 +1,38 @@
+// deck.gl-community
+// SPDX-License-Identifier: MIT
+// Copyright (c) vis.gl contributors
+
+export type { GeoArrowArcLayerProps } from "./layers/arc-layer.js";
+export { GeoArrowArcLayer } from "./layers/arc-layer.js";
+
+export type { GeoArrowColumnLayerProps } from "./layers/column-layer.js";
+export { GeoArrowColumnLayer } from "./layers/column-layer.js";
+
+export type { GeoArrowH3HexagonLayerProps as _GeoArrowH3HexagonLayerProps } from "./layers/h3-hexagon-layer.js";
+export { GeoArrowH3HexagonLayer as _GeoArrowH3HexagonLayer } from "./layers/h3-hexagon-layer.js";
+
+export type { GeoArrowHeatmapLayerProps } from "./layers/heatmap-layer.js";
+export { GeoArrowHeatmapLayer } from "./layers/heatmap-layer.js";
+
+export type { GeoArrowPathLayerProps } from "./layers/path-layer.js";
+export { GeoArrowPathLayer } from "./layers/path-layer.js";
+
+export type { GeoArrowPointCloudLayerProps } from "./layers/point-cloud-layer.js";
+export { GeoArrowPointCloudLayer } from "./layers/point-cloud-layer.js";
+
+export type { GeoArrowPolygonLayerProps } from "./layers/polygon-layer.js";
+export { GeoArrowPolygonLayer } from "./layers/polygon-layer.js";
+
+export type { GeoArrowScatterplotLayerProps } from "./layers/scatterplot-layer.js";
+export { GeoArrowScatterplotLayer } from "./layers/scatterplot-layer.js";
+
+export type { GeoArrowSolidPolygonLayerProps } from "./layers/solid-polygon-layer.js";
+export { GeoArrowSolidPolygonLayer } from "./layers/solid-polygon-layer.js";
+
+export type { GeoArrowTextLayerProps as _GeoArrowTextLayerProps } from "./layers/text-layer.js";
+export { GeoArrowTextLayer as _GeoArrowTextLayer } from "./layers/text-layer.js";
+
+export type { GeoArrowTripsLayerProps } from "./layers/trips-layer.js";
+export { GeoArrowTripsLayer } from "./layers/trips-layer.js";
+
+export type { GeoArrowPickingInfo } from "./types.js";
