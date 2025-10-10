198b87fb3aac63bb15568c3c12d9f861e6aa7206
Kyle Barron <kyle@developmentseed.org>
2025-10-10 12:39:46 -0400

    feat: Add vectorized props from polygon layer to h3 hexagon layer (#157)

--- packages/deck.gl-layers/src/layers/h3-hexagon-layer.ts
+++ packages/deck.gl-layers/src/layers/h3-hexagon-layer.ts
@@ -14,7 +14,7 @@ import type { H3HexagonLayerProps } from "@deck.gl/geo-layers";
 import { H3HexagonLayer } from "@deck.gl/geo-layers";
 import * as arrow from "apache-arrow";
 
-import { GeoArrowPickingInfo } from "../types";
+import { ColorAccessor, FloatAccessor, GeoArrowPickingInfo } from "../types";
 import { assignAccessor, extractAccessorsFromProps } from "../utils/utils";
 import { GeoArrowExtraPickingProps, getPickingInfo } from "../utils/picking";
 import { validateAccessors } from "../utils/validate";
@@ -25,6 +25,7 @@ export type GeoArrowH3HexagonLayerProps = Omit<
   "data" | "getHexagon"
 > &
   _GeoArrowH3HexagonLayerProps &
+  // Omit<GeoArrowPolygonLayerProps, "getPolygon"> &
   CompositeLayerProps;
 
 /** Props added by the GeoArrowH3HexagonLayer */
@@ -35,6 +36,31 @@ type _GeoArrowH3HexagonLayerProps = {
    * Called for each data object to retrieve the quadkey string identifier.
    */
   getHexagon: arrow.Data<arrow.Utf8>;
+
+  /** Fill color accessor.
+   * @default [0, 0, 0, 255]
+   */
+  getFillColor?: ColorAccessor;
+
+  /** Stroke color accessor.
+   * @default [0, 0, 0, 255]
+   */
+  getLineColor?: ColorAccessor;
+
+  /**
+   * Line width value of accessor.
+   * @default 1
+   */
+  getLineWidth?: FloatAccessor;
+
+  /**
+   * Elevation value of accessor.
+   *
+   * Only used if `extruded: true`.
+   *
+   * @default 1000
+   */
+  getElevation?: FloatAccessor;
 
   /**
    * If `true`, validate the arrays provided (e.g. chunk lengths)
@@ -55,7 +81,9 @@ const ourDefaultProps = {
   _validate: true,
 };
 
+// @ts-expect-error getFillColor
 const defaultProps: DefaultProps<GeoArrowH3HexagonLayerProps> = {
+  // ..._polygonDefaultProps,
   ..._defaultProps,
   ...ourDefaultProps,
 };
@@ -90,8 +118,6 @@ export class GeoArrowH3HexagonLayer<
       "getHexagon",
     ]);
 
-    const hexValues = hexData.values;
-
     const props: H3HexagonLayerProps = {
       // Note: because this is a composite layer and not doing the rendering
       // itself, we still have to pass in our defaultProps
@@ -106,8 +132,8 @@ export class GeoArrowH3HexagonLayer<
         length: hexData.length,
         attributes: {
           getHexagon: {
-            value: hexValues,
-            // h3 cells should always be 15 characters...?
+            value: hexData.values,
+            // h3 cells should always be 15 characters
             size: 15,
           },
         },
